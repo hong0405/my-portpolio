a334023e61b5a32d89f5b6c7c12a91c4625a5290
hong0405 <77203086+hong0405@users.noreply.github.com>
2023-12-22 09:48:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ async function sendEmail(to, subject, text) {
 }
 
 sendEmail("receiver_email@example.com", "Subject", "Hello World").catch(console.error);
+
+// 모든 링크 요소에 대한 이벤트 리스너를 등록
+var links = document.querySelectorAll("a");
+
+links.forEach(function(link) {
+  link.addEventListener("mouseover", function() {
+    var tooltip = document.createElement("div");
+    tooltip.className = "tooltip";
+    tooltip.textContent = "클릭하세요!"; // 원하는 툴팁 내용
+    document.body.appendChild(tooltip);
+
+    // 툴팁을 링크 위에 표시하기 위해 위치 조정
+    var linkRect = link.getBoundingClientRect();
+    tooltip.style.top = linkRect.top - 30 + "px"; // 링크 위에서 30px 위에 표시
+    tooltip.style.left = linkRect.left + "px";
+
+    // 툴팁 숨기기
+    link.addEventListener("mouseout", function() {
+      tooltip.remove();
+    });
+  });
+});
